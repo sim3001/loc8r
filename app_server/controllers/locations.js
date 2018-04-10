e3846c200ef56e8346fedcd2c61ad24c5d9d984a
Simranjit Singh <simranjit.boyal@gmail.com>
2018-04-10 01:20:55 -0600

    Added navbar to layout template

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,12 +1,12 @@
 /* GET 'home' Page. */
-module.exports.homeList = (res,req) => {
+module.exports.homeList = (req,res) => {
     res.render('index',{ title: 'Home'});
 };
 /* GET 'Location Info' Page. */
-module.exports.locationInfo = (res,req) => {
+module.exports.locationInfo = (req,res) => {
     res.render('index',{ title: 'Location Info'});
 };
 /* GET 'Add Review' Page. */
-module.exports.addReview = (res,req) => {
+module.exports.addReview = (req,res) => {
     res.render('index',{ title: 'Add Review'});
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,3 @@
-module.exports.about = (req,res)=> {
+module.exports.about = (req,res) => {
     return res.render('index',{title: 'About'});
 };
