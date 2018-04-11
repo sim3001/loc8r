e0fbbdfb5722cf7981b1d9f090b049280521630a
Simranjit Singh <simranjit.boyal@gmail.com>
2018-04-11 02:55:17 -0600

    Decoupled Data from template to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,11 @@
 /* GET 'home' Page. */
 module.exports.homeList = (req,res) => {
-    res.render('locations-list',{ title: 'Home'});
+    res.render('locations-list',{ title: 'Loc8r - find a place to work with wifi',
+pageHeader: {
+    title: 'Loc8r',
+    strapline: 'Find places to work with wifi near you!'
+        }
+    });
 };
 /* GET 'Location Info' Page. */
 module.exports.locationInfo = (req,res) => {
